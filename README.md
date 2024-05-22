7ef2cfe00cf8dd6ebfe8edca585f3b28e373b260
Cbrnex <taiko1577@gmail.com>
2024-05-22 11:58:04 +0900

    fix readme links and fix astro config

--- README.md
+++ README.md
@@ -1,11 +1,8 @@
 # Anysome  
 
   カテゴリ問わずのAwesomeリスト。  
-  Awesome list that included any categories.  
-
-  原文を日本語で書くが、英語への翻訳もしたい。  
-  gonna write in Japanese but wanna translate to English.  
 
 ## List
 
-- [Android Apps](categories/android-app.md)
+- [Android Apps](src/pages/list/android-app.md)
+- [Frontend](src/pages/list/frontend.md)
--- astro.config.ts
+++ astro.config.ts
@@ -1,21 +1,21 @@
 import { defineConfig } from "astro/config";
 
-import path from "path";
+// import path from "path";
 // import path from 'node:path'
 
 export default defineConfig({
-  vite: {
-    // css: {
-    //   preprocessorOptions: {
-    //     sass: {
-    //       additionalData: '\n@use "@/styles/api.sass" as api\n',
-    //     },
-    //   },
-    // },
-    resolve: {
-      alias: {
-        "@": path.resolve(__dirname, "src"),
-      },
-    },
-  },
+  // vite: {
+  //   css: {
+  //     preprocessorOptions: {
+  //       sass: {
+  //         additionalData: '\n@use "@/styles/api.sass" as api\n',
+  //       },
+  //     },
+  //   },
+  //   resolve: {
+  //     alias: {
+  //       "@": path.resolve(__dirname, "src"),
+  //     },
+  //   },
+  // },
 });
