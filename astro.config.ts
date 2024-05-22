a6195c5d627080c7f25cb37361088fa282091d28
Cbrnex <taiko1577@gmail.com>
2024-05-22 10:42:32 +0900

    add astro page

---
+++ astro.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from 'astro/config'
+
+// import path from 'path'
+
+// import path from 'node:path'
+
+export default defineConfig({
+  // vite: {
+  //   css: {
+  //     preprocessorOptions: {
+  //       sass: {
+  //         imports: path.resolve(__dirname, './src/styles/global.sass')
+  //       },
+  //     },
+  //   },
+  // },
+})
